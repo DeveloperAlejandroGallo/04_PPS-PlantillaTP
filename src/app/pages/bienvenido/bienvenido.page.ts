b15e3fd101680dc927c7d1cbc99a4b5ef3d0323c
Alejandro Gallo <alegallo@gmail.com>
2020-09-27 19:07:23 -0300

    con icon y menu

--- src/app/pages/bienvenido/bienvenido.page.ts
+++ src/app/pages/bienvenido/bienvenido.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BienvenidoPage implements OnInit {
 
   constructor() { }
-
+  title = 'Primera Pagina';
   ngOnInit() {
   }
 
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { Platform } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 
-import { FirecloudService } from "../../services/firecloud.service";
-import { FireauthService } from "../../services/fireauth.service";
+import { FirecloudService } from '../../services/firecloud.service';
+import { FireauthService } from '../../services/fireauth.service';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"],
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
   // Declaraciones
+  title = 'Login';
   correo: string;
   clave: string;
   mensaje: string;
@@ -27,14 +28,14 @@ export class LoginPage implements OnInit {
 
   loguear(user: string) {
     switch (user) {
-      case "admin": {
-        this.correo = "admin@admin.com";
-        this.clave = "123456";
+      case 'admin': {
+        this.correo = 'admin@admin.com';
+        this.clave = '123456';
         break;
       }
-      case "generico": {
-        this.correo = "generico@generico.com";
-        this.clave = "123456";
+      case 'generico': {
+        this.correo = 'generico@generico.com';
+        this.clave = '123456';
         break;
       }
     }
@@ -47,34 +48,34 @@ export class LoginPage implements OnInit {
 
   public validarCorreoClave(): void {
     if (this.correoValido(this.correo)) {
-      if (this.clave !== "" && this.clave !== undefined) {
+      if (this.clave !== '' && this.clave !== undefined) {
         this.authServise
           .logueoConEmailYClave(this.correo, this.clave)
           .then((resp) => {
-            this.mensaje = "Bienvenido";
-            this.router.navigate(["/bienvenido"]);
+            this.mensaje = 'Bienvenido';
+            this.router.navigate(['/bienvenido']);
           })
           .catch((error) => {
             console.log(error.code);
             switch (error.code) {
-              case "auth/invalid-email":
-                this.mensaje = "Correo con formato incorrecto";
+              case 'auth/invalid-email':
+                this.mensaje = 'Correo con formato incorrecto';
                 break;
-              case "auth/wrong-password":
-                this.mensaje = "Clave incorrecta";
+              case 'auth/wrong-password':
+                this.mensaje = 'Clave incorrecta';
                 break;
-              case "auth/user-not-found":
-                this.mensaje = "El usuario no existe.";
+              case 'auth/user-not-found':
+                this.mensaje = 'El usuario no existe.';
                 break;
               default:
                 this.mensaje = error.message;
             }
           });
       } else {
-        this.mensaje = "Por favor ingrese una clave";
+        this.mensaje = 'Por favor ingrese una clave';
       }
     } else {
-      this.mensaje = "Por favor ingrese un correo valido";
+      this.mensaje = 'Por favor ingrese un correo valido';
     }
   }
 
@@ -84,34 +85,34 @@ export class LoginPage implements OnInit {
       .then((res) => {
         console.log(res);
 
-        this.mensaje = "Registro exitoso.";
-        this.router.navigate(["login"]);
+        this.mensaje = 'Registro exitoso.';
+        this.router.navigate(['login']);
       })
       .catch((error) => {
         console.log(error);
         switch (error.code) {
-          case "auth/weak-password":
-            this.mensaje = "La clave debe poseer al menos 6 caracteres";
+          case 'auth/weak-password':
+            this.mensaje = 'La clave debe poseer al menos 6 caracteres';
             break;
-          case "auth/email-already-in-use":
-            this.mensaje = "Correo ya registrado";
+          case 'auth/email-already-in-use':
+            this.mensaje = 'Correo ya registrado';
             break;
-          case "auth/invalid-email":
-            this.mensaje = "Correo con formato invalido";
+          case 'auth/invalid-email':
+            this.mensaje = 'Correo con formato invalido';
             break;
-          case "auth/argument-error":
-            this.mensaje = "Correo con debe ser una cadena v\u00E1lida";
+          case 'auth/argument-error':
+            this.mensaje = 'Correo con debe ser una cadena v\u00E1lida';
             break;
           default:
-            this.mensaje = "Error en registro";
+            this.mensaje = 'Error en registro';
         }
       });
   }
 
   correoValido(correo: string) {
     let ret = true;
-    if (correo !== "" && correo !== undefined) {
-      if (!correo.includes("@")) {
+    if (correo !== '' && correo !== undefined) {
+      if (!correo.includes('@')) {
         ret = false;
       }
     } else {
